edd6bbba38acf7e1693a3e3ac4d5d694ba84aee3
rossnhorgan98 <ross.horgan1998@gmail.com>
2019-11-14 10:41:59 +0000

    Iteration 1

---
+++ FYP/Controllers/CitiesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FYP.Models;
+
+namespace FYP.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private readonly InventoryContext _context;
+
+        public CitiesController(InventoryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cities
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cities.ToListAsync());
+        }
+
+        // GET: Cities/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cities = await _context.Cities
+                .FirstOrDefaultAsync(m => m.CityId == id);
+            if (cities == null)
+            {
+                return NotFound();
+            }
+
+            return View(cities);
+        }
+
+        // GET: Cities/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CityId,Name,Country,CostOfLivingIndex,ClimateType,Population,NoOfUniversities,NoOfAirports,LightRailLines,TrafficRate,HomicideRate,IncomeTax,Vat,CorporationTax")] Cities cities)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cities);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cities);
+        }
+
+        // GET: Cities/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cities = await _context.Cities.FindAsync(id);
+            if (cities == null)
+            {
+                return NotFound();
+            }
+            return View(cities);
+        }
+
+        // POST: Cities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("CityId,Name,Country,CostOfLivingIndex,ClimateType,Population,NoOfUniversities,NoOfAirports,LightRailLines,TrafficRate,HomicideRate,IncomeTax,Vat,CorporationTax")] Cities cities)
+        {
+            if (id != cities.CityId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cities);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CitiesExists(cities.CityId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cities);
+        }
+
+        // GET: Cities/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cities = await _context.Cities
+                .FirstOrDefaultAsync(m => m.CityId == id);
+            if (cities == null)
+            {
+                return NotFound();
+            }
+
+            return View(cities);
+        }
+
+        // POST: Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var cities = await _context.Cities.FindAsync(id);
+            _context.Cities.Remove(cities);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CitiesExists(long id)
+        {
+            return _context.Cities.Any(e => e.CityId == id);
+        }
+    }
+}
---
+++ FYP/Models/InventoryContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FYP.Models
+{
+    public partial class InventoryContext : DbContext
+    {
+        public InventoryContext()
+        {
+        }
+
+        public InventoryContext(DbContextOptions<InventoryContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cities> Cities { get; set; }
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP2;Database=Inventory;Integrated Security=True");
+            }
+        }*/
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cities>(entity =>
+            {
+                entity.HasKey(e => e.CityId)
+                    .HasName("PK__Cities__F2D21A965DEB1641");
+
+                entity.Property(e => e.CityId).HasColumnName("CityID");
+
+                entity.Property(e => e.ClimateType)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CorporationTax).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.CostOfLivingIndex).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.Country)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.HomicideRate).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.IncomeTax).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TrafficRate).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.Vat)
+                    .HasColumnName("VAT")
+                    .HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
